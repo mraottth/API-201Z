b3114f3bd2c8cb6d4cf0912da0366a8837de8cb2
mraottth <64610726+mraottth@users.noreply.github.com>
2021-11-05 00:15:23 -0400

    Create global_variables.py

---
+++ Python/global_variables.py
@@ -0,0 +1,9 @@
+# Import libraries from libraries.py
+
+warnings.filterwarnings('ignore') 
+
+# Set start and end date params, n offset days, save_images flag
+start_date = '2021-07-01'
+end_date = '2021-10-31'
+n = 0 # Days to offset vax response by
+save_images = True
