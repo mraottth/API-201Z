98ec22eaeb56a1cc3c10d522c9141fc1147b1153
mraottth <64610726+mraottth@users.noreply.github.com>
2021-11-06 01:54:29 -0400

    Update plot_daily.py
    adding agg_lm plots

--- Python/plot_daily.py
+++ Python/plot_daily.py
@@ -252,3 +252,81 @@ lm(
     yformat='percent',
     xformat='percent'
 )
+
+
+# Plot aggregatd lms
+
+# full date window no splits
+agg_lm(
+    groupby=None,
+    hue_levels={"Total":'gray'},    
+    suptitle='Each point is 1 day aggregated nationally'
+)
+
+# Full date window split on party
+agg_lm(
+    groupby='2020 Election Winner',
+    hue_levels={'Democrat':'blue', 'Republican':'red'},
+    suptitle='Each point is 1 day aggregated by red or blue state'
+)
+
+# Full date window split on svi
+agg_lm(
+    groupby='SVI Bucket',
+    hue_levels={'High to Very High':'red', 'Moderate':'gold', 'Very Low to Low':'green'},
+    suptitle='Each point is 1 day aggregated by state-level SVI'
+)
+
+# Cases rising date window no split
+agg_lm(
+    groupby=None,
+    hue_levels={"Total":'gray'},    
+    suptitle='Each point is 1 day aggregated nationally',
+    start="2021-07-01",
+    end="2021-09-01"
+)
+
+# Cases rising date window split on party
+agg_lm(
+    groupby='2020 Election Winner',
+    hue_levels={'Democrat':'blue', 'Republican':'red'},
+    suptitle='Each point is 1 day aggregated by red or blue state',
+    start="2021-07-01",
+    end="2021-09-01"
+)
+
+# Cases rising date window split on svi
+agg_lm(
+    groupby='SVI Bucket',
+    hue_levels={'High to Very High':'red', 'Moderate':'gold', 'Very Low to Low':'green'},
+    suptitle='Each point is 1 day aggregated by state-level SVI',
+    start="2021-07-01",
+    end="2021-09-01"
+)
+
+# Cases falling date window no split
+agg_lm(
+    groupby=None,
+    hue_levels={"Total":'gray'},    
+    suptitle='Each point is 1 day aggregated nationally',
+    start="2021-09-01",
+    end="2021-10-31"
+)
+
+# Cases falling date window split on party
+agg_lm(
+    groupby='2020 Election Winner',
+    hue_levels={'Democrat':'blue', 'Republican':'red'},
+    suptitle='Each point is 1 day aggregated by red or blue state',
+    start="2021-09-01",
+    end="2021-10-31"
+)
+
+# Cases falling date window split on svi
+agg_lm(
+    groupby='SVI Bucket',
+    hue_levels={'High to Very High':'red', 'Moderate':'gold', 'Very Low to Low':'green'},
+    suptitle='Each point is 1 day aggregated by state-level SVI',
+    start="2021-09-01",
+    end="2021-10-31"
+)
