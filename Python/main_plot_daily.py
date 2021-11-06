9c36d0aaeab3db9e73054ac596ab9adc1c13ae49
Matt Roth <matthew.adam.roth@gmail.com>
2021-11-06 12:28:09 -0400

    split main plotting

--- Python/main_plot_daily.py
+++ Python/main_plot_daily.py
@@ -19,9 +19,9 @@ lm(
         + START_DATE + ' to ' + END_DATE + ' -- Each point is one state on one day'\
         , 1.2, 25),
     xlabel='Week-over-week growth in cases (% of cumulative)',
-    ylabel='% of remaining unvaxed pop. receiving jab in past week'
-    # yformat='percent',
-    # xformat='percent'
+    ylabel='% of remaining unvaxed pop. receiving jab in past week',
+    yformat='percent',
+    xformat='percent'
 )
 
 # Plot correlations between cases and vax split by party
@@ -251,91 +251,4 @@ lm(
     ylabel='% of remaining unvaxed pop. receiving jab in past week',
     yformat='percent',
     xformat='percent'
-)
-
-
-# Plot aggregatd lms
-
-# full date window no splits
-agg_lm(
-    data=df_joined_cases,
-    groupby=None,
-    hue_levels={"Total":'gray'},    
-    suptitle='Each point is 1 day aggregated nationally'
-)
-
-# Full date window split on party
-agg_lm(
-    data=df_joined_cases,
-    groupby='2020 Election Winner',
-    hue_levels={'Democrat':'blue', 'Republican':'red'},
-    suptitle='Each point is 1 day aggregated by red or blue state'
-)
-
-# Full date window split on svi
-agg_lm(
-    data=df_joined_cases,
-    groupby='SVI Bucket',
-    hue_levels={'High to Very High':'red', 'Moderate':'gold', 'Very Low to Low':'green'},
-    suptitle='Each point is 1 day aggregated by state-level SVI'
-)
-
-# Cases rising date window no split
-agg_lm(
-    data=df_joined_cases,
-    groupby=None,
-    hue_levels={"Total":'gray'},    
-    suptitle='Each point is 1 day aggregated nationally',
-    start="2021-07-01",
-    end="2021-09-01"
-)
-
-# Cases rising date window split on party
-agg_lm(
-    data=df_joined_cases,
-    groupby='2020 Election Winner',
-    hue_levels={'Democrat':'blue', 'Republican':'red'},
-    suptitle='Each point is 1 day aggregated by red or blue state',
-    start="2021-07-01",
-    end="2021-09-01"
-)
-
-# Cases rising date window split on svi
-agg_lm(
-    data=df_joined_cases,
-    groupby='SVI Bucket',
-    hue_levels={'High to Very High':'red', 'Moderate':'gold', 'Very Low to Low':'green'},
-    suptitle='Each point is 1 day aggregated by state-level SVI',
-    start="2021-07-01",
-    end="2021-09-01"
-)
-
-# Cases falling date window no split
-agg_lm(
-    data=df_joined_cases,
-    groupby=None,
-    hue_levels={"Total":'gray'},    
-    suptitle='Each point is 1 day aggregated nationally',
-    start="2021-09-01",
-    end="2021-10-31"
-)
-
-# Cases falling date window split on party
-agg_lm(
-    data=df_joined_cases,
-    groupby='2020 Election Winner',
-    hue_levels={'Democrat':'blue', 'Republican':'red'},
-    suptitle='Each point is 1 day aggregated by red or blue state',
-    start="2021-09-01",
-    end="2021-10-31"
-)
-
-# Cases falling date window split on svi
-agg_lm(
-    data=df_joined_cases,
-    groupby='SVI Bucket',
-    hue_levels={'High to Very High':'red', 'Moderate':'gold', 'Very Low to Low':'green'},
-    suptitle='Each point is 1 day aggregated by state-level SVI',
-    start="2021-09-01",
-    end="2021-10-31"
 )
