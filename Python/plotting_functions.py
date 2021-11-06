f803188857b5c8f441058aa861a333839ea6d2ff
Matt Roth <matthew.adam.roth@gmail.com>
2021-11-06 13:26:42 -0400

    add kde to agg_jp

--- Python/plotting_functions.py
+++ Python/plotting_functions.py
@@ -291,7 +291,8 @@ def agg_jp(data, groupby, hue_levels, suptitle, start=START_DATE, end=END_DATE):
             x='WoW_%_cases', 
             y='WoW_%_vax',                   
             hue=groupby, 
-            palette=list(hue_levels.values()),        
+            palette=list(hue_levels.values()),      
+            kind='kde'  
             )
     
     # Set chart parameters    
