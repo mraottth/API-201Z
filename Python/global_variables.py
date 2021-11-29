2c45d31aa6cb4ded516303426a40381f39d29a65
Matt Roth <matthew.adam.roth@gmail.com>
2021-11-29 12:07:53 -0500

    updated data

--- Python/global_variables.py
+++ Python/global_variables.py
@@ -4,8 +4,8 @@ from libraries import *
 warnings.filterwarnings('ignore') 
 
 # Set start and end date params, n offset days, save_images flag
-START_DATE = '2021-07-01'
-END_DATE = '2021-10-31'
+START_DATE = '2021-07-12'
+END_DATE = '2021-10-28'
 N = 0 # Days to offset vax response by
 SAVE_IMAGES = True
-REFRESH_ALL_DATA = False
+REFRESH_ALL_DATA = True
